1bc79f4e45ccd7065049c1b4203910cbdf2b0dbf
rachelchxn <85042944+rachelchxn@users.noreply.github.com>
2023-03-18 12:10:51 -0400

    REAL TIME OBJ DETECTION

---
+++ src/utilities.js
@@ -0,0 +1,20 @@
+export const drawRect = (detections, ctx) => {
+    detections.forEach(prediction => {
+        // get prediction
+        const [x,y,width,height] = prediction['bbox']
+        const text = prediction['class']
+
+        // set styling
+        const color = 'green'
+        ctx.strokeStyle = color
+        ctx.font = '24px Poppins'
+        ctx.fillStyle = color
+
+        // draw rects and text
+        ctx.beginPath()
+        ctx.fillText(text, x, y)
+        ctx.rect(x, y, width, height)
+        ctx.stroke()
+
+    })
+}
