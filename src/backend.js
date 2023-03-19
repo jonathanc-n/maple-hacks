e55e0e12a732b6e4b1db83207268671bb9602aa8
xanderchinxyz <xanderchin@gmail.com>
2023-03-19 02:25:10 -0400

    new api key

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const OpenAI = require('openai');
 const { Configuration, OpenAIApi } = OpenAI;
 
@@ -10,16 +12,17 @@ const port = 3001;
 
 const configuration = new Configuration({
     organization: "org-dCV8SSKqMUHKT533nPn1mM4r",
-    apiKey: "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"
+    apiKey: process.env.OPENAI_API_KEY
 });
 const openai = new OpenAIApi(configuration);
 
 app.use(bodyParser.json())
-app.use(cors());
+app.use(cors())
 
 app.post('/', async (req, res) => {
     // console.log("Hi")
     // console.log(message.string)
+    
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: req.body.string,
